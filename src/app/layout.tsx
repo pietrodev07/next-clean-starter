c095c8e6181dc351036c54fb3b63df5422b5da22
pietrodev07 <pietro.dev.07@gmail.com>
2025-04-13 10:04:32 +0200

    chore: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import "./globals.css";
+import { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import { PropsWithChildren } from "react";
+import { Providers } from "@/components/providers";
+import { Bricolage_Grotesque } from "next/font/google";
+import { ThemeToggle } from "@/components/theme-toggle";
+
+const bricolageFont = Bricolage_Grotesque({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nextjs Clean Starter",
+  description: "Nextjs Clean Starter",
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(bricolageFont.className, "antialiased")}>
+        <Providers>
+          {children}
+          <ThemeToggle />
+        </Providers>
+      </body>
+    </html>
+  );
+}
