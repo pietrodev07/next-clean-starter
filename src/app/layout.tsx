a9f1ff006e7124ad1e14c119ea5c264896884876
pietrodev07 <pietro.dev.07@gmail.com>
2025-05-11 10:13:04 +0200

    chore(font): change app font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,19 @@
 import "./globals.css";
 import { Metadata } from "next";
-import { cn } from "@/lib/utils";
 import { PropsWithChildren } from "react";
 import { metadatInfo } from "@/lib/metadata";
+import { Space_Grotesk } from "next/font/google";
 import { Providers } from "@/components/providers";
-import { Bricolage_Grotesque } from "next/font/google";
 import { ThemeToggle } from "@/components/theme-toggle";
 
-const bricolageFont = Bricolage_Grotesque({ subsets: ["latin"] });
+const appFont = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = metadatInfo;
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn(bricolageFont.className, "antialiased")}>
+      <body className={appFont.className}>
         <Providers>
           {children}
           <ThemeToggle />
